8503b44c8299dd7c672e136fd8fad6f7268e7193
baxter <baxter.demers@gmail.com>
2020-02-18 10:32:52 -0500

    update the downloader

--- get_csv_from_db.py
+++ get_csv_from_db.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 
 import config
+from state_po_lst import state_po_lst
 
 print("initiating DB connection...")
 myConnection = psycopg2.connect(host=config.db['hostname'], user=config.db['username'], password=config.db['password'], dbname=config.db['database'])
@@ -12,25 +13,25 @@ curr = myConnection.cursor()
 print("connected... ")
 
 table_name = 'geo_tbl'
-state_abbreviation = 'NJ'
 
-querry = "SELECT * FROM {} LIMIT 0;".format(table_name)
+query = "SELECT * FROM {} LIMIT 0;".format(table_name)
 print("Executing Query")
-curr.execute(querry)
+curr.execute(query)
 colnames = [desc[0] for desc in curr.description]
 print(colnames)
 
-querry = "SELECT * FROM {} WHERE state = '{}' ORDER BY id ASC;".format(table_name, state_abbreviation)
-print("Executing Query")
-start = time.time()
-curr.execute(querry)
+for state_po in state_po_lst:
+    query = "SELECT * FROM {} WHERE state = '{}' ORDER BY id ASC;".format(table_name, state_po)
+    print("Executing Query: ", query)
+    start = time.time()
+    curr.execute(query)
 
-rows = curr.fetchall()
-df = pd.DataFrame(rows, columns=colnames)
-df = df.set_index('id')
+    rows = curr.fetchall()
+    df = pd.DataFrame(rows, columns=colnames)
+    df = df.set_index('id')
 
-print(df.head())
-df.to_csv('geocoded_{}.csv'.format(state_abbreviation))
+    print(df.head())
+    df.to_csv('geocoded_{}.csv'.format(state_po))
 
 myConnection.commit()
 curr.close()
